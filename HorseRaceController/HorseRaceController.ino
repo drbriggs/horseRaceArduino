35a933c79c59272fa91d7ff5d229b788e76c797e
Dan <briggs.daniel.r@gmail.com>
2022-05-22 14:10:25 -0600

    Add files via upload
    Initial upload of current files

---
+++ HorseRaceController/HorseRaceController.ino
@@ -0,0 +1,89 @@
+#define TO_LANES    2
+#define GAME_LIGHTS 13
+#define GAME_BUTTON 12
+#define FROM_LANE1  7
+#define FROM_LANE2  8
+#define FROM_LANE3  9
+#define LED_LANE1   3
+#define LED_LANE2   5
+#define LED_LANE3   6
+
+
+
+void setup() {
+  Serial.begin(9600);
+  // Configure Control Pin and set it low
+  pinMode(TO_LANES, OUTPUT);
+  digitalWrite(TO_LANES, LOW);
+  Serial.println("Set TO_LANES Low");
+  // Configure pins to read game status from lane nanos
+  pinMode(FROM_LANE1, INPUT);
+  pinMode(FROM_LANE2, INPUT);
+  pinMode(FROM_LANE3, INPUT);
+  // Configure pins for game controls
+  pinMode(GAME_BUTTON, INPUT_PULLUP);
+  pinMode(GAME_LIGHTS, OUTPUT);
+  digitalWrite(GAME_LIGHTS, LOW);
+  // Configure Lane Winner Lights
+  pinMode(LED_LANE1, OUTPUT);
+  pinMode(LED_LANE2, OUTPUT);
+  pinMode(LED_LANE3, OUTPUT);
+  
+  flashLed(3);
+}
+
+int gameState = LOW;
+int newButtonVal;
+int oldButtonVal = 1; 
+int isWinner = 0;
+
+void loop() {
+  // Check the game play button state
+  newButtonVal = digitalRead(GAME_BUTTON);
+  // If the game is not currently running
+  if (gameState == LOW){
+    // If the button was pressed start the game
+    if (oldButtonVal == 1 && newButtonVal == 0){
+      gameState = HIGH;
+      digitalWrite(GAME_LIGHTS, HIGH);
+      isWinner = 0;
+    }
+  } else { // The game is currently running
+    //Check for a winner
+    if (digitalRead(FROM_LANE1) == HIGH){
+      //TODO:  Player 1 WON!! do something about it
+      digitalWrite(LED_LANE1, HIGH);
+      isWinner = 1;
+    }
+    if (digitalRead(FROM_LANE2) == HIGH){
+      //TODO:  Player 2 WON!! do something about it
+      digitalWrite(LED_LANE1, HIGH);
+      isWinner = 2;
+    }
+    if (digitalRead(FROM_LANE3) == HIGH){
+      //TODO:  Player 3 WON!! do something about it
+      digitalWrite(LED_LANE1, HIGH);
+      isWinner = 3;
+    }
+    //TODO:  If someone won, send signal to reset the game
+    if (isWinner > 0) {
+      //Is there something to do here about who won?
+      //Turn the game off
+      gameState = LOW;
+    }
+  }
+  oldButtonVal = newButtonVal;
+}
+
+void setWinner(int winnerLane) {
+  
+}
+
+void flashLed(int numFlash) {
+  for (int i = 0; i < numFlash; i++) { 
+    digitalWrite(13, HIGH);
+    delay(250);
+    digitalWrite(13, LOW);
+    delay(250);
+  }
+}
---
+++ HorseRaceSensor/HorseRaceSensor.ino
@@ -0,0 +1,85 @@
+#include <Stepper.h>
+
+#define FROM_CONTROLLER   7 //7 
+#define TO_CONTROLLER     8  //8
+#define SENSOR1           4
+#define SENSOR2           5
+#define SENSOR3           6
+
+#define STEPS_PER_REV  2048
+#define MOT_SPEED         14
+#define TOTAL_STEPS 40UL*2048
+
+int stepsToTurn;
+int newSensor1val;
+int newSensor2val;
+int newSensor3val;
+int oldSensor1val = 1;
+int oldSensor2val = 1;
+int oldSensor3val = 1;
+int stepsTaken = 0;
+int old_controller = LOW;
+
+Stepper myStepper(STEPS_PER_REV, 10,12,11,13);
+
+void setup() {
+  // put your setup code here, to run once:
+  myStepper.setSpeed(MOT_SPEED);
+  // TODO: set sensors 1-3 as input
+  pinMode(SENSOR1, INPUT_PULLUP);
+  pinMode(SENSOR2, INPUT_PULLUP);
+  pinMode(SENSOR3, INPUT_PULLUP);
+  pinMode(FROM_CONTROLLER, INPUT);
+  pinMode(TO_CONTROLLER, OUTPUT);
+  digitalWrite(TO_CONTROLLER, LOW);
+}
+
+void loop() {
+  // if game is running
+  if(FROM_CONTROLLER == HIGH){
+    // readSensors
+    newSensor1val = digitalRead(SENSOR1);
+    newSensor2val = digitalRead(SENSOR2);
+    newSensor3val = digitalRead(SENSOR3);
+    // if the sensors have recently changed, add turns to step counter
+    if (oldSensor1val == 1 && newSensor1val == 0) {
+      stepsToTurn += STEPS_PER_REV;
+    }
+    if (oldSensor2val == 1 && newSensor2val == 0) {
+      stepsToTurn += STEPS_PER_REV;
+    }
+    if (oldSensor3val == 1 && newSensor3val == 0) {
+      stepsToTurn += STEPS_PER_REV;
+    }
+    // if there is still a value on step counter, turn the motor
+    if (stepsToTurn > 0) {
+      myStepper.step(1);
+      stepsTaken++;
+      stepsToTurn--;
+    }
+    // if horse has reached the end of the track, notify the controller
+    if (stepsTaken >= TOTAL_STEPS) {
+      digitalWrite(TO_CONTROLLER, HIGH);
+    }
+    // set old sensor value to current value for next cycle
+    oldSensor1val = newSensor1val;
+    oldSensor2val = newSensor2val;
+    oldSensor3val = newSensor3val;
+  }
+  // check if game has recently ended
+  // This is interesting to know, but what do we do with it?
+  if (old_controller == LOW && digitalRead(FROM_CONTROLLER) == HIGH){
+    //digitalWrite(TO_CONTROLLER, HIGH);
+  }
+  //if game not running
+  if(digitalRead(FROM_CONTROLLER) == LOW){
+    //and horse not in starting possition 
+    if(stepsTaken > 0){
+      //move horse back to start
+      myStepper.step(-1);
+      stepsTaken--;
+    }
+  }
+  //keep track of controller value for next cycle
+  old_controller = digitalRead(FROM_CONTROLLER);
+}
